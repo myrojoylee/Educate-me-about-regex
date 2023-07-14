b49f3a7b4dc7d82a55c016ab38f494daeeebc85d
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-14 14:37:44 -0400

    coded userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,16 @@
-const router = require('express').Router();
-const {}
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
