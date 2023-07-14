aca64eca09815ae0b812e1a057c91156e054665b
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-14 16:46:12 -0400

    added post and delete uri for friends and reactions

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/userController");
 
 // /api/users
@@ -12,5 +14,11 @@ router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route("/:userId/friends").post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").delete(removeFriend);
 
 module.exports = router;
